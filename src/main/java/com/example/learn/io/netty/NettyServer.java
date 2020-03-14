e5bb8332a5913f7f79445dce302a503bdf75969d
tao19930415 <1181639720@qq.com>
2020-03-14 15:33:26 +0800

    添加pipeline使用注释说明

--- src/main/java/com/example/learn/io/netty/NettyServer.java
+++ src/main/java/com/example/learn/io/netty/NettyServer.java
@@ -29,6 +29,10 @@ public class NettyServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            //添加channel时，有出栈和入栈的的区别；以客户端为例，当给客户端发送一条数据。这时对于客户端就是入栈。
+                            //这时的pipeline的顺序是从head到tail ,而且是只执行inBandHandel。就是decoder-->NettyServerHandel(解码-->接收)
+                            //如果是客户端发送一条时间给服务端，这时对于客户端是出栈操作，pipeline的执行顺序是从tail到head
+                            //而且只执行outBandHandel.就是NettyServerHandel-->encoder(发送-->编码);
                             ChannelPipeline pipeline = socketChannel.pipeline();
                             pipeline.addLast("decoder",new StringDecoder());
                             pipeline.addLast("encoder",new StringEncoder());
@@ -36,6 +40,7 @@ public class NettyServer {
                         }
                     });
             System.out.println("netty server start");
+            //sync()表示同步等待绑定端口执行完才执行后面的操作
             ChannelFuture cf = bootstrap.bind(8088).sync();
             cf.channel().closeFuture().sync();
         } finally {
