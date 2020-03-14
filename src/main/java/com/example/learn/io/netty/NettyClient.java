a41c2c1c2b5a21c117c0a4da83ea9a560cab2dca
tao19930415 <1181639720@qq.com>
2020-03-14 15:07:31 +0800

    netty实现的简单聊天功能

--- src/main/java/com/example/learn/io/netty/NettyClient.java
+++ src/main/java/com/example/learn/io/netty/NettyClient.java
@@ -26,8 +26,8 @@ public class NettyClient {
                 @Override
                 protected void initChannel(Channel channel) throws Exception {
                     ChannelPipeline pipeline = channel.pipeline();
-                    pipeline.addLast("decoder", new StringDecoder());
-                    pipeline.addLast("encoder", new StringEncoder());
+                    pipeline.addLast("decoder", new StringDecoder());//解码
+                    pipeline.addLast("encoder", new StringEncoder());//编码
                     pipeline.addLast(new NettyClientHandel());
                 }
             };
